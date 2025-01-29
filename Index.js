d9caff5954e859d8c0e49d59709eb786a86798a2
Admin <sohilasobhysadek@gmail.com>
2025-01-29 13:59:02 +0200

    language updates

--- Index.js
+++ Index.js
@@ -3,13 +3,18 @@ AOS.init();
 document.querySelector('.navbar-toggler').addEventListener('click', function () {
     const menu = document.getElementById('navbarSupportedContent');
     menu.classList.add('show');
-    menu.classList.add('animate__fadeInLeft');
 });
 document.getElementById('closeMenu').addEventListener('click', function () {
     const menu = document.getElementById('navbarSupportedContent');
     menu.classList.remove('show');
-    menu.classList.remove('animate__fadeInLeft');
-
 });
+
+function switchtoEN() {
+    window.location.href = "Index.html";
+}
+function switchtoAr() {
+    window.location.href = "IndexAr.html";
+}
+
 
 
