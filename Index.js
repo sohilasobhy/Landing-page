031ebf95b3506a809c4ac4d51288f3aaa04fb599
Admin <sohilasobhysadek@gmail.com>
2025-01-31 17:09:34 +0200

    updates 31/1

--- Index.js
+++ Index.js
@@ -2,11 +2,15 @@
 AOS.init();
 document.querySelector('.navbar-toggler').addEventListener('click', function () {
     const menu = document.getElementById('navbarSupportedContent');
+    const filter = document.getElementById('body');
     menu.classList.add('show');
+    filter.classList.add('show');
 });
 document.getElementById('closeMenu').addEventListener('click', function () {
     const menu = document.getElementById('navbarSupportedContent');
+    const filter = document.getElementById('body');
     menu.classList.remove('show');
+    filter.classList.remove('show');
 });
 
 function switchtoEN() {
